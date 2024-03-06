6a907b22f96c33cd15bc86445ac26ede9f6d7c28
KinslayerHG <elesis517@gmail.com>
2024-03-06 23:29:52 +0000

    just moddifing .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <><h1> Um texto de amor</h1><></>
+        <p>Nos versos do meu coração, floresce o amor que sinto por você.Cada momento ao seu lado é uma poesia escrita pelas estrelas, uma sinfonia de sentimentos que embalam nossos sonhos.</p>
+
+        <p>Seus olhos são como portais para um universo de ternura e paixão, onde me perco e me encontro em um eterno encantamento. Sua presença é como o sol que aquece minha alma, iluminando os dias mais sombrios com sua luz.</p>
+
+        <p>Em cada gesto, em cada toque, encontro a essência de um amor verdadeiro, que transcende as palavras e se manifesta em cada batida do meu coração. Você é a minha inspiração, a minha musa, a razão pela qual meu mundo ganha cor e significado.</p>
+
+        <p>Ao seu lado, descubro o verdadeiro significado da felicidade, da cumplicidade, do companheirismo. Juntos, somos como dois pássaros voando em perfeita harmonia, rumo ao horizonte onde nossos sonhos se tornam realidade.</p>
+        
+        <p>Te amarei hoje, amanhã e para sempre, pois em você encontrei a minha morada, a minha paz, o meu eterno amor. E que a nossa história seja escrita com os mais belos capítulos de amor e felicidade. Eu te amo, para sempre e sempre.</p></>
+        
 }
 
 export default Home; 
